dcdd21c9331154831da057ccee3c62582076677e
fgdhsfgb bgggbbgfb <vmanen68@gmail.com>
2021-12-17 14:10:35 +0100

    Added the Homepage component

---
+++ src/pages/home.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    render() { 
+        return ( 
+            <React.Fragment>
+                <div>
+                    Hello world
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default Home;
